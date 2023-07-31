aa1b7449e3bcdeef5a8cc90b39892aae31c5e568
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2023-07-31 20:54:28 +0530

    updated resume data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import { sharedDataDummy } from "./data/your-data";
+import { resumeDataDummy } from "./data/your-data";
 
 const App = () => {
   const [resumeData, setResumeData] = useState({});
@@ -37,29 +39,12 @@ const App = () => {
     }
   };
 
-  const loadResumeFromPath = (path) => {
-    fetch(path)
-      .then((response) => response.json())
-      .then((data) => {
-        setResumeData(data);
-      })
-      .catch((error) => {
-        // alert(error.message);
-        setResumeData(resumeDataDummy)
-      });
+  const loadResumeFromPath = () => {
+    setResumeData(resumeDataDummy)
   };
 
   const loadSharedData = () => {
-    fetch(`portfolio_shared_data.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setSharedData(data);
-        document.title = `${data.basic_info.name}`;
-      })
-      .catch((error) => {
-        // alert(error.message);
-        setSharedData(sharedDataDummy)
-      });
+    setSharedData(sharedDataDummy)
   };
 
   useEffect(() => {
@@ -111,189 +96,4 @@ const App = () => {
 
 export default App;
 
-const resumeDataDummy = {
-  "basic_info": {
-    "description_header": "Hi",
-    "description": "👋 I'm Davina Griss. Fictional person for preview purposes :) I'm working with newest front-end frameworks like Angular, React and Vue. What you are seeing now is portfolio template from Dorota1997. If you like this portfolio template, make sure to ⭐ the repository to make it more recognizable for other users. Thank you 💜",
-    "section_name": {
-      "about": "About me",
-      "projects": "Projects",
-      "skills": "Skills",
-      "experience": "Experience"
-    }
-  },
-  "projects": [
-    {
-      "title": "Animal Shelter",
-      "startDate": "2020",
-      "description": "The most expanded application I had opportunity to work with. I've learned many technologies and my code was reviewed by awesome curator. Application handles all adoption processess and allows to store all evidence on adopting animals from animal shelter.",
-      "images": [
-        "images/portfolio/animal-shelter/p1.jpg",
-        "images/portfolio/animal-shelter/p2.jpg"
-      ],
-      "url": "",
-      "technologies": [
-        {
-          "class": "devicon-angularjs-plain",
-          "name": "Angular"
-        },
-        {
-          "class": "devicon-typescript-plain",
-          "name": "TypeScript"
-        },
-        {
-          "class": "devicon-csharp-plain",
-          "name": "C#"
-        }
-      ]
-    },
-    {
-      "title": "Photography",
-      "startDate": "2018",
-      "description": "Personal project for study subject. I was responsible for testing photography application that optimizes images with popular algorithms used by graphic editors like Pixlr or Adobe Photoshop. I've earned A grade :)",
-      "images": [
-        "images/portfolio/photography/p1.jpg",
-        "images/portfolio/photography/p2.jpg"
-      ],
-      "url": "https://github.com",
-      "technologies": [
-        {
-          "class": "devicon-react-original",
-          "name": "React"
-        },
-        {
-          "class": "devicon-javascript-plain",
-          "name": "JavaScript"
-        }
-      ]
-    },
-    {
-      "title": "3D Object Viewer",
-      "startDate": "2015",
-      "description": "One of the first apps I was working on my internship. I had to develop front-end implementation for app that shows 3D models of known buildings. This was also my first project in Angular framework. I've learned a lot!",
-      "images": [
-        "images/portfolio/adventure/p1.jpg",
-        "images/portfolio/adventure/p2.jpg"
-      ],
-      "url": "https://github.com",
-      "technologies": [
-        {
-          "class": "devicon-angularjs-plain",
-          "name": "Angular"
-        },
-        {
-          "class": "devicon-typescript-plain",
-          "name": "TypeScript"
-        },
-        {
-          "class": "devicon-csharp-plain",
-          "name": "C#"
-        }
-      ]
-    }
-  ],
-  "experience": [
-    {
-      "company": "DefOpenSource",
-      "title": "Front-End Developer",
-      "years": "10.2019 - present",
-      "mainTech": [
-        "Angular 8/9/10"
-      ],
-      "technologies": [
-        "REST API",
-        "RxJS",
-        "JavaScript",
-        "Bootstrap",
-        "MDBootstrap",
-        "EF Core",
-        ".NET Core",
-        "SignalR",
-        "Angular Material"
-      ]
-    },
-    {
-      "company": "Serros Solutions",
-      "title": "Intern",
-      "years": "01.2018 - 09.2019",
-      "mainTech": [
-        "Angular 7/8"
-      ],
-      "technologies": [
-        "RxJS",
-        "Django",
-        "PHP",
-        "JavaScript",
-        "DHTMLX Gantt"
-      ]
-    }
-  ]
-}
-const sharedDataDummy = {
-  "basic_info": {
-    "name": "Davina Griss",
-    "titles": ["Front-end Developer", "Senior Data Engineer", "Dev Team lead", "Mobile App Developer"],
-    "social": [
-      {
-        "name": "github",
-        "url": "https://github.com",
-        "class": "fab fa-github"
-      },
-      {
-        "name": "instagram",
-        "url": "https://www.instagram.com",
-        "class": "fab fa-instagram"
-      }
-    ],
-    "image": "myProfile.jpg"
-  },
-  "skills": {
-    "icons": [
-      {
-        "name": "HTML 5",
-        "class": "devicon-html5-plain",
-        "level": "95"
-      },
-      {
-        "name": "CSS 3",
-        "class": "devicon-css3-plain",
-        "level": "95"
-      },
-      {
-        "name": "Angular",
-        "class": "devicon-angularjs-plain",
-        "level": "80"
-      },
-      {
-        "name": "TypeScript",
-        "class": "devicon-typescript-plain",
-        "level": "90"
-      },
-      {
-        "name": "JavaScript",
-        "class": "devicon-javascript-plain",
-        "level": "70"
-      },
-      {
-        "name": "Sass",
-        "class": "devicon-sass-original",
-        "level": "75"
-      },
-      {
-        "name": "Bootstrap",
-        "class": "devicon-bootstrap-plain",
-        "level": "85"
-      },
-      {
-        "name": "C#",
-        "class": "devicon-csharp-plain",
-        "level": "65"
-      },
-      {
-        "name": "MySql",
-        "class": "devicon-mysql-plain",
-        "level": "60"
-      }
-    ]
-  }
-}
+
---
+++ src/data/your-data.js
@@ -0,0 +1,258 @@
+export const resumeDataDummy = {
+    "basic_info": {
+        "description_header": "Hi",
+        "description": "👋 I'm Umesh Savaliya. I'm a Software Developer with experience in working with front-end frameworks like React, Angular, and TypeScript. I have a passion for creating user-friendly web applications and solving complex problems. Feel free to reach out to me at +91 9904130013 or umeshsavaliya007@gmail.com. You can also find me on LinkedIn at linkedin.com/in/umesh-savaliya and check out my GitHub at github.com/Umesh-daiict. Thank you! 💜",
+        "section_name": {
+            "about": "About me",
+            "projects": "Projects",
+            "skills": "Skills",
+            "experience": "Experience"
+        }
+    },
+    "projects": [
+        {
+            "title": "Fudu",
+            "startDate": "Jan. 2023",
+            "description": "Collaborated closely with UI/UX designers to translate design mockups and wireframes into pixel-perfect frontend implementations with Mobile Responsive Web App to be used by Restaurant Owners to grow their business. Integrated third-party APIs and libraries, such as Google Maps, social media integrations (Apple and Google Login), seamlessly into the frontend, expanding the project’s functionalities and enhancing Restaurant Owner engagement to its customers. Implemented robust testing strategies, including unit testing using Jest and Enzyme, to ensure the reliability and stability of the frontend codebase, resulting in a more robust and bug-free application.",
+            "images": [],
+            "url": "",
+            "technologies": [
+                {
+                    "class": "devicon-react-original",
+                    "name": "React"
+                },
+                {
+                    "class": "devicon-typescript-plain",
+                    "name": "TypeScript"
+                },
+                {
+                    "class": "devicon-materialui-plain",
+                    "name": "Material-UI"
+                },
+                {
+                    "class": "devicon-jest-plain",
+                    "name": "Jest"
+                },
+                {
+                    "class": "devicon-enzyme-plain",
+                    "name": "Enzyme"
+                }
+                // Add more technologies as needed
+            ]
+        },
+        {
+            "title": "STEER.GLOBAL",
+            "startDate": "Jul. 2022",
+            "endDate": "Feb. 2023",
+            "description": "Worked as a software developer on a platform designed to safeguard students’ mental health. Contributed to the redesign of the platform to provide a better user experience. Implemented Redux States to manage application-wide state and reduced application load time to 1.5 seconds. Successfully migrated a medium-sized application from AngularJS to React within 3 months, despite limited resources due to unstable funding. Added features to manage student and teacher data, set permissions, and enable file upload/export capabilities for a school with a population of over 5000.",
+            "images": [],
+            "url": "",
+            "technologies": [
+                {
+                    "class": "devicon-react-original",
+                    "name": "React"
+                },
+                {
+                    "class": "devicon-typescript-plain",
+                    "name": "TypeScript"
+                },
+                {
+                    "class": "devicon-redux-original",
+                    "name": "Redux"
+                },
+                {
+                    "class": "devicon-reactrouter-plain",
+                    "name": "React-Router"
+                },
+                {
+                    "class": "devicon-materialui-plain",
+                    "name": "Material-UI"
+                },
+                {
+                    "class": "devicon-tailwindcss-plain",
+                    "name": "Tailwind CSS"
+                }
+                // Add more technologies as needed
+            ]
+        },
+        {
+            "title": "IFFCO Tokio",
+            "startDate": "March 2021",
+            "endDate": "Sep. 2021",
+            "description": "As a software developer, I worked on the IFFCO-TOKIO app which offered policyholders the ability to buy and renew motor, health, travel, home, and trade insurance policies. Implemented forms and document verification features to manage twice the amount of documents compared to the previous version, improving the overall efficiency of the app. Developed and maintained the policy management validation process, streamlined the document management process, and reduced 3000 lines of code from the codebase to enhance the overall user experience.",
+            "images": [],
+            "url": "",
+            "technologies": [
+                {
+                    "class": "devicon-angularjs-plain",
+                    "name": "Angular"
+                },
+                {
+                    "class": "devicon-typescript-plain",
+                    "name": "TypeScript"
+                },
+                {
+                    "class": "devicon-angularmaterial-plain",
+                    "name": "Angular Material"
+                },
+                {
+                    "class": "devicon-reactrouter-plain",
+                    "name": "React-Router"
+                },
+                {
+                    "class": "devicon-rxjs-plain",
+                    "name": "RxJS"
+                }
+                // Add more technologies as needed
+            ]
+        }
+        // Add more projects as needed
+    ],
+    "experience": [
+        {
+            "company": "Skyline Infosys",
+            "title": "Software Developer",
+            "years": "March 2022 - Present",
+            "mainTech": [
+                "React",
+                "TypeScript",
+                "TailwindCSS",
+                "Material UI"
+            ],
+            "technologies": [
+                "STEER.GLOBAL",
+                "Redux",
+                "Google Maps API",
+                "MySQL",
+                "phpMyAdmin",
+                "Wonde API"
+                // Add more technologies as needed
+            ]
+        },
+        {
+            "company": "Unknown Company",
+            "title": "Junior Software Developer",
+            "years": "September 2021 - February 2022",
+            "mainTech": [
+                "Node.js",
+                "Express",
+                "Material-UI",
+                "Formik",
+                "React Bootstrap",
+                "Antd"
+            ],
+            "technologies": [
+                "REST APIs",
+                "React",
+                "Material-UI",
+                "Formik",
+                "React Bootstrap",
+                "Antd"
+                // Add more technologies as needed
+            ]
+        },
+        {
+            "company": "Clomotech",
+            "title": "Software Engineer Intern",
+            "years": "March 2021 - September 2021",
+            "mainTech": [
+                "Angular 6"
+            ],
+            "technologies": [
+                "Angular Forms",
+                "Angular HTTP",
+                "Angular Material",
+                "Bootstrap",
+                "Chart.js",
+                "moment.js",
+                "Fontawesome"
+                // Add more technologies as needed
+            ]
+        },
+        {
+            "company": "DA-IICT",
+            "title": "Summer Research Intern",
+            "years": "May 2020 - August 2020",
+            "mainTech": [
+                "C++"
+            ],
+            "technologies": [
+                "CYK Algorithm",
+                "Dynamic Programming",
+                "Chomsky Normal Form (CNF)"
+                // Add more technologies as needed
+            ]
+        }
+        // Add more experience entries as needed
+    ]
+};
+
+
+export const sharedDataDummy = {
+    "basic_info": {
+        "name": "Umesh Savaliya",
+        "titles": ["Software Developer", "React Enthusiast", "Tech Enthusiast"],
+        "social": [
+            {
+                "name": "github",
+                "url": "https://github.com/Umesh-daiict",
+                "class": "fab fa-github"
+            },
+            {
+                "name": "linkedin",
+                "url": "https://www.linkedin.com/in/umesh-savaliya",
+                "class": "fab fa-linkedin"
+            }
+        ],
+        "image": "myProfile.jpg"
+    },
+    "skills": {
+        "icons": [
+            {
+                "name": "HTML 5",
+                "class": "devicon-html5-plain",
+                "level": "95"
+            },
+            {
+                "name": "CSS 3",
+                "class": "devicon-css3-plain",
+                "level": "95"
+            },
+            {
+                "name": "React",
+                "class": "devicon-react-original",
+                "level": "90"
+            },
+            {
+                "name": "TypeScript",
+                "class": "devicon-typescript-plain",
+                "level": "90"
+            },
+            {
+                "name": "JavaScript",
+                "class": "devicon-javascript-plain",
+                "level": "70"
+            },
+            {
+                "name": "Sass",
+                "class": "devicon-sass-original",
+                "level": "75"
+            },
+            {
+                "name": "Bootstrap",
+                "class": "devicon-bootstrap-plain",
+                "level": "85"
+            },
+            {
+                "name": "C#",
+                "class": "devicon-csharp-plain",
+                "level": "65"
+            },
+            {
+                "name": "MySQL",
+                "class": "devicon-mysql-plain",
+                "level": "60"
+            }
+        ]
+    }
+};
