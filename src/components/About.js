02ee50699ffc0b9e5840c7ae6b84ec2887f120d4
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2023-08-22 22:51:51 +0530

    tring to fix about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { Icon } from "@iconify/react";
-import angularIcon from "@iconify/icons-logos/angular-icon";
-import reactIcon from "@iconify/icons-logos/react";
-import nodeJsIcon from "@iconify/icons-logos/nodejs-icon";
+import React from 'react';
+import { Icon } from '@iconify/react';
+import angularIcon from '@iconify/icons-logos/angular-icon';
+import reactIcon from '@iconify/icons-logos/react';
+import nodeJsIcon from '@iconify/icons-logos/nodejs-icon';
 import profilePic from '../assets/images/myProfile.jpg';
-import JsIcon from "@iconify/icons-logos/javascript";
-import typeIcon from "@iconify/icons-logos/typescript-icon";
+import JsIcon from '@iconify/icons-logos/javascript';
+import typeIcon from '@iconify/icons-logos/typescript-icon';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const About = (props) => {
-  let sectionName = "";
-  let hello = "";
-  let about = "";
-  let devName = "";
-
+  let sectionName = '';
+  let hello = '';
+  let about = '';
+  let devName = '';
 
   if (props.resumeBasicInfo) {
     sectionName = props.resumeBasicInfo.section_name.about;
@@ -22,83 +22,74 @@ const About = (props) => {
   }
 
   return (
-    <section id="about">
-      <div className="col-md-12">
-        <h1 style={{ color: "black" }}>
+    <section id='about'>
+      <div className='col-md-12'>
+        <h1 style={{ color: 'black' }}>
           <span>{sectionName}</span>
         </h1>
-        <div className="row center mx-auto mb-5">
-          <div className="col-md-4 mb-5 center">
-            <div className="polaroid">
-              <span style={{ cursor: "auto" }}>
-                <div>
-                <img height="250px" width={'350px'} src={profilePic} alt="Avatar placeholder" />
-                <p style={{ cursor: "auto", paddingTop: '10px', color: '', }} className="wave">{devName}</p>
-                <Icon icon={angularIcon} style={{ fontSize: "400%", margin: "9% 5% 0 5%" }} />
-                  <p
-                    className="text-center"
-                    style={{ color: 'black', fontSize: '18px', marginTop: "4px" }}
-                  >
-                    Angular
-                  </p>
-                </div>
-                <div>
-                <Icon icon={reactIcon} style={{ fontSize: "400%", margin: "9% 5% 0 5%" }} />
-                  <p
-                    className="text-center"
-                    style={{ color: 'black', fontSize: '18px', marginTop: "4px" }}
-                  >
-                    Angular
-                  </p>
-                </div>
-                <div> <Icon icon={nodeJsIcon} style={{ fontSize: "400%", margin: "9% 5% 0 5%" }} />
-                  <p
-                    className="text-center"
-                    style={{ color: 'black', fontSize: '18px', marginTop: "4px" }}
-                  >
-                    Angular
-                  </p>
-                </div>
-                <div> <Icon icon={JsIcon} style={{ fontSize: "400%", margin: "9% 5% 0 5%" }} />
-                  <p
-                    className="text-center"
-                    style={{ color: 'black', fontSize: '18px', marginTop: "4px" }}
-                  >
-                    Angular
-                  </p>
-                </div>
-                <div> <Icon icon={typeIcon} style={{ fontSize: "400%", margin: "9% 5% 0 5%" }} />
-                  <p
-                    className="text-center"
-                    style={{ color: 'black', fontSize: '18px', marginTop: "4px" }}
-                  >
-                    Angular
-                  </p>
-                </div>
+        <div className='row center mx-auto mb-5'>
+          <div className='col-md-4 mb-5 center'>
+            <div className='polaroid'>
+              <span>
+                <img
+                  height='250px'
+                  width={'350px'}
+                  src={profilePic}
+                  alt='Avatar placeholder'
+                />
+                <p
+                  style={{ cursor: 'auto', paddingTop: '10px', color: '' }}
+                  className='wave'>
+                  {devName}
+                </p>
+
+                <Row>
+                  {[
+                    { id: 1, icon: angularIcon, title: 'Angular' },
+                    { id: 2, icon: reactIcon, title: 'React' },
+                    { id: 3, icon: nodeJsIcon, title: 'NodeJs' },
+                    { id: 4, icon: JsIcon, title: 'Javascript' },
+                    { id: 5, icon: typeIcon, title: 'Typescript' },
+                  ].map((skill) => (
+                    <Col key={skill.id} xs={6} lg={4} className='mb-3'>
+                      <div className='text-center'>
+                        <Icon icon={skill.icon} style={{ fontSize: '400%' }} />
+                        <p className='mt-2' style={{ color: 'black', fontSize: '18px' }}>
+                          {skill.title}
+                        </p>
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
               </span>
             </div>
-            <div>
-          </div>
-
-            <div className="col-md-10">
-              <div className="card">
-                <div className="card-header">
-                  <span className="iconify" data-icon="emojione:red-circle" data-inline="false"></span>
+            <div className='col-md-10'>
+              <div className='card'>
+                <div className='card-header'>
+                  <span
+                    className='iconify'
+                    data-icon='emojione:red-circle'
+                    data-inline='false'></span>
                   &nbsp;
-                  <span className="iconify" data-icon="twemoji:yellow-circle" data-inline="false"></span>
+                  <span
+                    className='iconify'
+                    data-icon='twemoji:yellow-circle'
+                    data-inline='false'></span>
                   &nbsp;
-                  <span className="iconify" data-icon="twemoji:green-circle" data-inline="false"></span>
+                  <span
+                    className='iconify'
+                    data-icon='twemoji:green-circle'
+                    data-inline='false'></span>
                 </div>
                 <div
-                  className="card-body font-trebuchet text-justify ml-3 mr-3"
+                  className='card-body font-trebuchet text-justify ml-3 mr-3'
                   style={{
-                    height: "auto",
-                    fontSize: "132%",
-                    lineHeight: "200%",
-                  }}
-                >
+                    height: 'auto',
+                    fontSize: '132%',
+                    lineHeight: '200%',
+                  }}>
                   <br />
-                  <span className="wave">{hello} :) </span>
+                  <span className='wave'>{hello} :) </span>
                   <br />
                   <br />
                   {about}
