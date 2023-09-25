0c2fc901ae9b54b307c47fa63ce67b940956a140
Sun-007 <78736365+Umesh-daiict@users.noreply.github.com>
2023-09-25 21:55:16 +0530

    final connect secton

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -3,8 +3,7 @@ import { Button, Container, Form } from 'react-bootstrap';
 
 const Connect = () => {
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
+        title: '',
         message: '',
     });
     const handleChange = (e) => {
@@ -16,40 +15,47 @@ const Connect = () => {
     };
     console.log('aaaaaaaa', formData);
     const handleSendEmail = (e) => {
-        e.preventDefault()
+        e.preventDefault();
+
         const recipient = 'aaa@yopmail.com';
-        const subject = encodeURIComponent(formData.name);
+        const subject = encodeURIComponent(formData.title);
         const body = encodeURIComponent(formData.message);
+
+        // Construct the mailto link with subject and body
         const mailtoLink = `mailto:${recipient}?subject=${subject}&body=${body}`;
 
+        // Open the user's default email client with the mailto link
         window.location.href = mailtoLink;
     };
     return (
         <section id='about'>
             <div className='col-md-12 mx-auto'>
                 <h1 style={{ color: 'black' }}>
-                    {/* <span>{sectionName}</span> */}
                     <span>Contact Me</span>
                 </h1>
                 <div className='row center mx-auto mb-5'>
                     <Container style={{ maxWidth: '768px' }}>
                         <Form className="contact-form" onSubmit={handleSendEmail}>
-                            <Form.Group controlId="name">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your name" name="name" onChange={handleChange} />
+                            <Form.Group controlId="sendto">
+                                <Form.Label>To:-</Form.Label>
+                                <a href='mailto:aaa@yopmail.com' className='pl-2' title='mainlink' aria-label='mailto'>aaa@yopmail.com</a>
                             </Form.Group>
-                            <Form.Group controlId="email">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email" name="email" onChange={handleChange} />
+
+                            <Form.Group controlId="title">
+                                <Form.Label>Title</Form.Label>
+                                <Form.Control type="text" placeholder="Enter your title" name="title" onChange={handleChange} />
                             </Form.Group>
+
                             <Form.Group controlId="message">
                                 <Form.Label>Message</Form.Label>
                                 <Form.Control as="textarea" rows={4} placeholder="Enter your message" name="message" onChange={handleChange} />
                             </Form.Group>
                             <div className="text-center">
+                                {/* <a href={`mailto:aaa@yopmail.com?subject=${formData.name}&body=${formData.message}`}> */}
                                 <Button className="btn-custom" variant="primary" type="submit">
                                     Submit
                                 </Button>
+                                {/* </a> */}
                             </div>
                         </Form>
                     </Container>
